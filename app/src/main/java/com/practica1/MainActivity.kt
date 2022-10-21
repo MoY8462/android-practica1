b5a40ba7e5a98db658dfab4020f09af60998b6b6
MoY8462 <moy.sav21@hotmail.com>
2022-10-20 22:14:50 -0500

    style: :art: splash screen

--- app/src/main/java/com/practica1/MainActivity.kt
+++ app/src/main/java/com/practica1/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.practica1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
         setContentView(R.layout.activity_main)
+        Handler().postDelayed({
+            val i = Intent(
+                this@MainActivity,
+                MainActivity2::class.java
+            )
+            startActivity(i)
+            finish()
+        }, 3000)
     }
 }
