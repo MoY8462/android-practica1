de2820e1053f8204bd8ef4485d891bce3b5997d6
MoY8462 <moy.sav21@hotmail.com>
2022-10-20 22:55:10 -0500

    refactor: :recycle: features in splash screen

--- app/src/main/java/com/practica1/MainActivity.kt
+++ app/src/main/java/com/practica1/MainActivity.kt
@@ -3,24 +3,17 @@ package com.practica1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.view.WindowManager
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
         setContentView(R.layout.activity_main)
-        Handler().postDelayed({
-            val i = Intent(
-                this@MainActivity,
-                MainActivity2::class.java
-            )
-            startActivity(i)
+        thread{
+            Thread.sleep(3000)
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
             finish()
-        }, 3000)
+        }
     }
 }
