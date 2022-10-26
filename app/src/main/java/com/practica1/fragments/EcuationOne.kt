1f3971306a3a92aa2dcdb8dfd04f6a61f2ce7189
MoY8462 <moy.sav21@hotmail.com>
2022-10-26 08:43:53 -0500

    refactor: :recycle: validate forms in ecuation one

--- app/src/main/java/com/practica1/fragments/EcuationOne.kt
+++ app/src/main/java/com/practica1/fragments/EcuationOne.kt
@@ -1,14 +1,16 @@
 package com.practica1
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.practica1.databinding.FragmentEcuationOneBinding
+import java.math.RoundingMode
+import java.text.DecimalFormat
 
 class EcuationOne : Fragment() {
 
@@ -28,16 +30,34 @@ class EcuationOne : Fragment() {
         return binding.root
     }
 
+    @SuppressLint("StringFormatMatches")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.btnCalcular.setOnClickListener {
-            val radio = binding.numRadio.text.toString().toFloat()
-            val altura = binding.numAltura.text.toString().toFloat()
+            val radio = binding.numRadio.text.toString()
+            val altura = binding.numAltura.text.toString()
 
-            val volumen : Double = radio * 3.1416 * radio * altura
-            binding.txtResultado.text = getString(R.string.volumen, volumen.toString())
-            Toast.makeText(requireActivity(), "Numero $radio", Toast.LENGTH_LONG).show()
+            when {
+                radio.isEmpty() -> {
+                    //Toast.makeText(requireActivity(), "Ingresa el radio del cilindro", Toast.LENGTH_SHORT).show()
+                    binding.numRadio.error = "Ingresa el valor del radio"
+                }
+                altura.isEmpty() -> {
+                    //Toast.makeText(requireActivity(), "Ingresa la altura del cilindro", Toast.LENGTH_SHORT).show()
+                    binding.numAltura.error  = "Ingrsa el valor de la altura"
+                }
+                else -> {
+                    val volumen : Double = radio.toFloat()* altura.toFloat() * 3.1416 * radio.toFloat()
+                    val df = DecimalFormat("#.##")
+                    df.roundingMode = RoundingMode.DOWN
+                    val roundVolumen = df.format(volumen)
+                    binding.txtResultado.text = "V = ${roundVolumen.toString()} cm³" //getString(R.string.volumen,volumen)
+                    Toast.makeText(requireActivity(), "V = ${volumen.toString()} cm³", Toast.LENGTH_LONG).show()
+                }
+
+            }
+
         }
     }
     override fun onDestroy() {
@@ -46,3 +66,4 @@ class EcuationOne : Fragment() {
     }
 
 }
+
